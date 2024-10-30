ba072ea972eaacec4d68101cad82cb3384c90078
seon707 <163107407+seon707@users.noreply.github.com>
2024-10-30 14:53:46 +0900

    close.c

--- kvs_lab/close.c
+++ kvs_lab/close.c
@@ -3,5 +3,17 @@
 int close(kvs_t* kvs)
 {
 	/* do program */
+	node_t* current = kvs->db;
+	node_t* next;
+
+	while (current != NULL) {
+        	next = current->next;
+        	free(current->value); // Free the value
+		free(current); // Free the node
+        	current = next;
+    	}
+
+    	free(kvs);
+	/* do program */
 	return 0;
 }
