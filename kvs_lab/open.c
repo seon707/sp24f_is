be1937bea5d04a9786c7c7715ae00ef950537f86
seon707 <163107407+seon707@users.noreply.github.com>
2024-10-30 15:00:40 +0900

    open.c

--- kvs_lab/open.c
+++ kvs_lab/open.c
@@ -5,8 +5,12 @@ kvs_t* open()
 {
 	kvs_t* kvs = (kvs_t*) malloc (sizeof(kvs_t));
 
-	if(kvs)
+	if(kvs) {
 		kvs->items = 0;
+		/* 추가 코드 */
+        	kvs->db = NULL; // Initialize the database as empty
+	}
+	/* 추가 코드 */
 	printf("Open: kvs has %d items\n", kvs->items);
 
 	return kvs;
