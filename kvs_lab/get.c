19de527c2cb37c20dbc85148b8c1c78d5c5bae73
seon707 <163107407+seon707@users.noreply.github.com>
2024-10-30 14:52:43 +0900

    get.c

--- kvs_lab/get.c
+++ kvs_lab/get.c
@@ -3,7 +3,21 @@
 char* get(kvs_t* kvs, const char* key)
 {
 	/* do program here */
+	node_t* current = kvs->db;
 
+	while (current != NULL) {
+		if (strcmp(current->key, key) == 0) {
+            // Found the key, return the value
+			return strdup(current->value);
+		}
+		current = current->next;
+	}
+
+    // Key not found
+	printf("Key not found: %s\n", key);
+	return NULL;
+	/* do program here */
+	
 	char* value = (char*)malloc(sizeof(char)*100);
 
 	if(!value){
