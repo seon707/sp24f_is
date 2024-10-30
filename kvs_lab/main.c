3b6f75592c309035b246b61717a6d9116828b08b
seon707 <163107407+seon707@users.noreply.github.com>
2024-10-30 15:05:34 +0900

    main.c

--- kvs_lab/main.c
+++ kvs_lab/main.c
@@ -12,11 +12,40 @@ int main()
 	// workload execution  
 	
 	// 1) 	file read 
+	FILE* file = fopen("query.dat", "r");
+	if (!file) {
+		printf("Failed to open file\n");
+	        close(kvs);
+	        return -1;
+	    }
+	
+	char operation[10];
+	char key[100];
+	char value[100];
 	// 2) 	if put, insert (key, value) into kvs.
 	// 		if get, seach the key in kvs and return the value. 
 	//		Return -1 if the key is not found  
-
-
+	while (fscanf(file, "%s %s %s", operation, key, value) != EOF) {
+		if (strcmp(operation, "put") == 0) {
+            // put operation
+			if (put(kvs, key, value) == -1) {
+				printf("Failed to put key: %s\n", key);
+			}
+		} else if (strcmp(operation, "get") == 0) {
+            // get operation
+			char* result = get(kvs, key);
+			if (result) {
+				printf("Value for key %s: %s\n", key, result);
+				free(result);
+			} else {
+                // Return -1 if the key is not found
+				printf("Key %s not found\n", key);
+			}
+		}
+	}
+	
+	fclose(file);
+	/*edit end*/
 	close(kvs);
 	
 	return 0;
